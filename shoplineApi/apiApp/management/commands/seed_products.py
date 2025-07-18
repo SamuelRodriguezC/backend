81067f822e01a5f55706d687c16f5cf78b9095b6
SamuelRodriguezC <samuelrc250@gmail.com>
2025-07-18 12:57:48 -0500

    Arreglar seeder de productos

--- shoplineApi/apiApp/management/commands/seed_products.py
+++ shoplineApi/apiApp/management/commands/seed_products.py
@@ -12,38 +12,44 @@ class Command(BaseCommand):
         # Diccionario: nombre producto => (precio, categoría, imagen)
         products = {
             # Electrónica
-            'Apple Robotic Digital Camera': (150.000, 'Electrónica', 'apple-robotic-digital-camera-min.jpg'),
+            'cámara Digital Robótica': (150.000, 'Electrónica', 'apple-robotic-digital-camera-min.jpg'),
             'Smart Black V9': (450.000, 'Electrónica', 'smart-black-v9-min.jpg'),
-            'Sony 4K Digital Camera': (10200.000, 'Electrónica', 'sony-4k-digital-camera-min.jpg'),
-            'Samsung Gaming Pad': (300.00, 'Electrónica', 'samsung-gaming-pad-min.jpg'),
+            'Camara Sony 4K': (10200.000, 'Electrónica', 'sony-4k-digital-camera-min.jpg'),
+            'Video Grabadora': (300.00, 'Electrónica', 'simple-videogragher-set-min.jpg'),
+            'Control X-Box': (153.000, 'Electrónica', 'smasung-gaming-pad-min.jpg'),
 
             # Ropa
-            'Apple Packed T-Shirt': (35.000, 'Ropa', 'apple-packed-tshirt-min.jpg'),
-            'Baggy Sized Jeans': (59.000, 'Ropa', 'baggy-sized-jeans-min.jpg'),
-            'Milky Jacket': (120.000, 'Ropa', 'milky-jacket-min.jpg'),
-            'Gucci Purple Sweater': (450.500, 'Ropa', 'gucci-purple-sweater-min.jpg'),
-            'Female Blue Dress': (80.900, 'Ropa', 'female-blue-dress-min.jpg'),
+            'Camisa Azul Cuadros': (35.000, 'Ropa', 'apple-packed-tshirt-min.jpg'),
+            'Baggy Jeans': (59.000, 'Ropa', 'baggy-sized-jeans-min.jpg'),
+            'Chaqueta blanca Algodón': (120.000, 'Ropa', 'milky-jacket-min.jpg'),
+            'Gucci Buzo Azul': (450.500, 'Ropa', 'gucci-purple-sweater-min.jpg'),
+            'Vestido Verde para Mujer': (80.900, 'Ropa', 'female-blue-dress-min.jpg'),
+            'Abrigo Verde para Mujer': (80.900, 'Ropa', 'female-green-jacket-min.jpg'),
+            'Chaqueta Tommy hifliger': (452.000, 'Ropa', 'tommyhilfiger-sweaters-min.jpg'),
+            'Chaqueta de Cuadros Azul': (150.000, 'Ropa', 'gucci-clothing-set-min.jpg'),
 
             # Libros
-            'Elon Recommended Books': (49.700, 'Libros', 'elon-recommended-books-min.jpg'),
-            'Jeff Recommended Books': (39.000, 'Libros', 'jeff-recommed-books-min.jpg'),
-            'Trump Recommended Books': (45.300, 'Libros', 'trump-recommended-books-min.jpg'),
-            'Lord of the Rings': (60.100, 'Libros', 'lord-of-the-rings-min.jpg'),
+            'Libros Desarrollo Personal': (49.700, 'Libros', 'elon-recommended-books-min.jpg'),
+            'Libros de Historia': (39.000, 'Libros', 'jeff-recommed-books-min.jpg'),
+            'Libros Filosofía': (45.300, 'Libros', 'trump-recommended-books-min.jpg'),
+            'Libros Sociología': (60.100, 'Libros', 'lord-of-the-rings-min.jpg'),
+            'Libros Fisica': (475.000, 'Libros', 'hooked-book-min.jpg'),
 
             # Comida
-            'American Strawberry': (5.700, 'Comida', 'american-strawberry-min.jpg'),
-            'Chocolate Baked Cake': (15.300, 'Comida', 'chocolate-baked-cake-min.jpg'),
-            'Strawberry Chocolate Cake': (18.000, 'Comida', 'strawberry-chocolate-cake-min.jpg'),
-            'Coloured Macaroons': (8.400, 'Comida', 'coloured-macaroons-min.jpg'),
-            'Fried Macaroons': (7.750, 'Comida', 'fried-macaroons-min.jpg'),
-            'Fried Spaghetti': (12.400, 'Comida', 'fried-spaghetti-min.jpg'),
-            'Fried Fish with Vegetable': (22.400, 'Comida', 'fried-fish-with-vegetable-min.jpg'),
-            'Fried Chips with Burger': (9.99, 'Comida', 'fried-chips-with-burger-min.jpg'),
+            'Fresas Importadas': (5.700, 'Comida', 'american-strawberry-min.jpg'),
+            'Pastel de Chocolate': (15.300, 'Comida', 'chocolate-baked-cake-min.jpg'),
+            'Pastel de Fresas con chocolate': (18.000, 'Comida', 'strawberry-chocolate-cake-min.jpg'),
+            'Macarrones': (8.400, 'Comida', 'coloured-macaroons-min.jpg'),
+            'Macarroes Pasta': (7.750, 'Comida', 'fried-macaroons-min.jpg'),
+            'Spaghetti': (12.400, 'Comida', 'fried-spaghetti-min.jpg'),
+            'Pescado Frito con Vegetales': (22.400, 'Comida', 'fried-fish-with-vegetable-min.jpg'),
+            'Papas Fritas con Hamburguesa': (9.99, 'Comida', 'fired-chips-with-burger-min.jpg'),
 
             # Juguetes
-            'Amoured Caterpillar Trailer': (250.400, 'Juguetes', 'amoured_caterpillar-trailer-min.jpg'),
-            'Bumble B Toy Bus': (35.900, 'Juguetes', 'bumble-b-toy-bus-min.jpg'),
-            'Toy Trailer Vehicles': (45.300, 'Juguetes', 'toy-trailer-vehicles-min.jpg'),
+            'Remolque': (250.400, 'Juguetes', 'amoured_caterpillar-trailer-min.jpg'),
+            'Autobús de juguete Bumble B': (35.900, 'Juguetes', 'bumble-b-toy-bus-min.jpg'),
+            'Vehículos de juguete': (45.300, 'Juguetes', 'toy-trailer-vehicles-min.jpg'),
+            'Camioneta Tundra Juguete': (45.880, 'Juguetes', 'toy-tundra-car-min.jpg'),
         }
 
         image_base_path = os.path.join('media', 'product_img')
@@ -67,8 +73,7 @@ class Command(BaseCommand):
 
                 image_path = os.path.join(image_base_path, image_filename)
                 if os.path.exists(image_path):
-                    with open(image_path, 'rb') as img_file:
-                        product.image.save(image_filename, File(img_file), save=False)
+                    product.image.name = f'product_img/{image_filename}'
 
                 product.save()
                 self.stdout.write(self.style.SUCCESS(f'Producto "{name}" creado en la categoría "{category_name}"'))
